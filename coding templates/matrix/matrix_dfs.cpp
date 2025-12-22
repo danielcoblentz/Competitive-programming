4a8bd9375a97c1f986945d04fd0fd98110bcb2c8
Daniel Coblentz <danielcoblentz916@gmail.com>
2025-12-22 15:03:27 -0500

    Add BFS and DFS implementations for shortest path and path counting in matrices

---
+++ coding templates/matrix/matrix_dfs.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <algorithm>
+
+using std::vector;
+using std::min;
+
+// Count paths (backtracking)
+// In C++ it's easier to use a 2D array for visit rather than a hashset.
+int dfs(const vector<vector<int>>& grid, int r, int c, vector<vector<int>>& visit) {
+    if (grid.empty() || grid[0].empty()) return 0;
+    int ROWS = grid.size(), COLS = grid[0].size();
+    if (r < 0 || c < 0 || r >= ROWS || c >= COLS ||
+        visit[r][c] || grid[r][c]) {
+        return 0;
+    }
+    if (r == ROWS - 1 && c == COLS - 1) {
+        return 1;
+    }
+
+    visit[r][c] = 1;
+
+    int count = 0;
+    count += dfs(grid, r + 1, c, visit);
+    count += dfs(grid, r - 1, c, visit);
+    count += dfs(grid, r, c + 1, visit);
+    count += dfs(grid, r, c - 1, visit);
+
+    visit[r][c] = 0;
+    return count;
+}
